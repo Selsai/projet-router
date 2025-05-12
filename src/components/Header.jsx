4094c463a95477f99d94a6b2468b5c1b7d7b6b04
Amairi <selsabilamairi@gmail.com>
2025-05-12 16:44:45 +0200

    activité 6

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,43 @@
 import { Container, Nav, Navbar } from "react-bootstrap";
 import { NavLink } from "react-router";
+import "../styles/Header.css";
+
 
 const Header = () => {
-  return (
-    <header>
-      <Navbar bg="light" expand="lg">
-        <Container>
-          <Navbar.Toggle aria-controls="menu" />
-          <Navbar.Collapse id="menu">
-            <Nav className="ms-auto">
-              <Nav.Link as={NavLink} to="/">Accueil</Nav.Link>
-              <Nav.Link as={NavLink} to="/category/vetements">Vêtements</Nav.Link>
-              <Nav.Link as={NavLink} to="/category/chaussures">Chaussures</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </header>
-  );
-};
-
-export default Header;
+    return (
+      <header>
+        <Navbar bg="light" expand="lg">
+          <Container>
+            <Navbar.Toggle aria-controls="menu" />
+            <Navbar.Collapse id="menu">
+              <Nav className="ms-auto">
+                <Nav.Link
+                  as={NavLink}
+                  to="/"
+                  className={({ isActive }) => (isActive ? "active" : undefined)}
+                >
+                  Accueil
+                </Nav.Link>
+                <Nav.Link
+                  as={NavLink}
+                  to="/category/vetements"
+                  className={({ isActive }) => (isActive ? "active" : undefined)}
+                >
+                  Vêtements
+                </Nav.Link>
+                <Nav.Link
+                  as={NavLink}
+                  to="/category/chaussures"
+                  className={({ isActive }) => (isActive ? "active" : undefined)}
+                >
+                  Chaussures
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </header>
+    );
+  };
+  
+  export default Header;
