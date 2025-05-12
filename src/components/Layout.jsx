55455928ac61cac9e66815c78e7d3d9054b67e1d
Amairi <selsabilamairi@gmail.com>
2025-05-12 16:11:37 +0200

    activité 5 correction

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,16 +1,19 @@
 import Header from "./Header";
 import Footer from "./Footer";
 import { Outlet } from "react-router";
+import { Container } from "react-bootstrap";
 
 const Layout = () => {
   return (
-    <>
+    <div className="d-flex flex-column min-vh-100">
       <Header />
-      <main>
-        <Outlet />
+      <main className="flex-grow-1 my-4">
+        <Container>
+          <Outlet />
+        </Container>
       </main>
       <Footer />
-    </>
+    </div>
   );
 };
 
