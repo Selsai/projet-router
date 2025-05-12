067be3b2a3e0ea59bceea70444d7417cf4548efb
Amairi <selsabilamairi@gmail.com>
2025-05-12 12:29:20 +0200

    activités 1 & 2

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from "react-router";
+import Home from "./pages/Home.jsx";
+import Layout from "./components/Layout.jsx";
+
+function App() {
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
