e9428fa32f75957f1002385c1d6c2b737d10b7d4
Amairi <selsabilamairi@gmail.com>
2025-05-12 16:31:10 +0200

    activité 5 correction

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,15 +5,15 @@ import { Container } from "react-bootstrap";
 
 const Layout = () => {
   return (
-    <div className="d-flex flex-column min-vh-100">
+    <>
       <Header />
-      <main className="flex-grow-1 my-4">
+      <main className="my-4">
         <Container>
           <Outlet />
         </Container>
       </main>
       <Footer />
-    </div>
+    </>
   );
 };
 
